9c26e9a187d816339d821789914471656f671895
mrNoactiv <mrNoactiv@gmail.com>
2016-12-01 19:14:56 +0100

    dorovnání

--- app/src/main/java/com/example/gruti/frogger21/MainMenu.java
+++ app/src/main/java/com/example/gruti/frogger21/MainMenu.java
@@ -1,7 +1,10 @@
 package com.example.gruti.frogger21;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainMenu extends AppCompatActivity {
 
@@ -9,5 +12,29 @@ public class MainMenu extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+
+        //exit apky
+        Button bExit = (Button) findViewById(R.id.bExit);
+        bExit.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                finish();
+                System.exit(0);
+            }
+        });
+    }
+
+    //Vytvoření nové hry
+    public void ClickNewGame(View view) {
+        Intent LunchNewGame=new Intent(this,GameActivity.class);
+        startActivity(LunchNewGame);
+    }
+
+    //zobrazeni skore
+    public void ClickScore(View view) {
+        Intent ShowScore=new Intent(this,Score.class);
+        startActivity(ShowScore);
+
     }
 }
